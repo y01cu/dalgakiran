9daa3b7b1b9ec8d030919fae09a98791f8735dcc
Umut Kaan Özdemir <umutaan50@gmail.com>
2023-05-07 18:08:42 +0300

    Added project files
    Let's start.

---
+++ Assets/Scripts/Others/Character.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour, ProcessInformation
+{
+    string name;
+
+    public void ProcessImage()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ProcessText()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ProcessVideo()
+    {
+        throw new System.NotImplementedException();
+    }
+    // This class used to be abstract.
+    // Character used to have ProcessInformation interface with it.
+
+}
---
+++ Assets/Scripts/PCManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PCManager : MonoBehaviour
+{
+    public GameObject socialMediaApp;
+    private bool isSocialMediaAppActive = false;
+
+    public GameObject messagingApp;
+    private bool isMessagingAppActive = false;
+
+    public GameObject imageEditingApp;
+    private bool isImageEditingAppActive = false;
+
+    public GameObject videoEditingApp;
+    private bool isVideoEditingAppActive = false;
+
+    public void SocialMediaAppButton()
+    {
+        if (isSocialMediaAppActive == false)
+        {
+            socialMediaApp.SetActive(true);
+            isSocialMediaAppActive = true;
+            return;
+        }
+        socialMediaApp.SetActive(false);
+        isSocialMediaAppActive = false;
+
+    }
+
+    public void MessagingAppButton()
+    {
+        if (isMessagingAppActive == false)
+        {
+            messagingApp.SetActive(true);
+            isMessagingAppActive = true;
+            return;
+        }
+        messagingApp.SetActive(false);
+        isMessagingAppActive = false;
+    }
+
+    public void ImageEditingAppButton()
+    {
+        if (isImageEditingAppActive == false)
+        {
+            imageEditingApp.SetActive(true);
+            isImageEditingAppActive = true;
+            return;
+        }
+        imageEditingApp.SetActive(false);
+        isImageEditingAppActive = false;
+    }
+
+    public void VideoEditingAppButton()
+    {
+        if (isVideoEditingAppActive == false)
+        {
+            videoEditingApp.SetActive(true);
+            isVideoEditingAppActive = true;
+            return;
+        }
+        videoEditingApp.SetActive(false);
+        isVideoEditingAppActive = false;
+    }
+}
